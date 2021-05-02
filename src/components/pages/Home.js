36aedbd2ef6cd4cbfba56e62c2ae1114beaf289d
Mohammad Shoumik Raihan <37881170+Z0G@users.noreply.github.com>
2021-05-03 03:46:38 +0600

    Completed the project

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,6 +2,7 @@ import '../../App.css';
 import HeroSection from "../HeroSection";
 import React from 'react';
 import Cards from "../Cards";
+import Footer from "../Footer";
 
 const Home = () => {
     return (
